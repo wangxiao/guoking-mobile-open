6d8c01b248036c4b7d60bc28660a88ab1be2a4fe
尼奥 <1988wangxiao@gmail.com>
2014-09-13 17:37:07 +0800

    [feature] 国京手机版开户。

---
+++ app/scripts/main.js
@@ -0,0 +1,65 @@
+(function() {
+    'use strict';
+
+    var errorEle = $('#show-error');
+    var mobileEle = $('#mobile');
+    var nameCnEle = $('#name-cn');
+
+    mobileEle.on('focus', function() {
+        mobileEle.removeClass('error');
+    });
+    nameCnEle.on('focus', function() {
+        nameCnEle.removeClass('error');
+    });
+
+    function checkMobile() {
+        var val = mobileEle.val().trim();
+        if (!val) {
+            errorEle.text('请填写手机号码');
+            mobileEle.addClass('error');
+            return false;
+        } else if (/\D/.test(val)) {
+            errorEle.text('手机号只能是数字');
+            mobileEle.addClass('error');
+            return false;
+        } else if (val.length !== 11) {
+            errorEle.text('手机号码位数不对');
+            mobileEle.addClass('error');
+            return false;            
+        } else {
+            mobileEle.removeClass('error');
+            return true;
+        }
+    }
+
+    function checkNameCn() {
+        var val = nameCnEle.val().trim();
+        if (!val) {
+            errorEle.text('请填写您的姓名');
+            nameCnEle.addClass('error');
+            return false;
+        } else if (!/^[\u4e00-\u9fa5]+$/.test(val)) {
+            errorEle.text('请填写中文姓名');
+            nameCnEle.addClass('error');     
+            return false;       
+        } else {
+            nameCnEle.removeClass('error');
+            return true;
+        }
+    }
+
+    function submit() {
+        $.post('/account/register_simple', {
+            nameCn: nameCnEle.val().trim(),
+            mobile: mobileEle.val().trim()
+        });
+    }
+
+    $('#open-btn').on('click', function() {
+        if (checkMobile() && checkNameCn()) {
+            submit();
+            $('#step1').hide();
+            $('#step2').show();
+        }
+    });
+})();
